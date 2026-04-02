0c1f191208834374a980cc4752cefafd4e252365
Joey <kirimaki@daum.net>
2026-04-02 20:53:11 +0900

    feat: 로깅 logback 전역 설정 (추적, 로그레벨, 롤링)

---
+++ src/main/java/com/f5hell/common/logging/LogFilter.java
@@ -0,0 +1,44 @@
+package com.f5hell.common.logging;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class LogFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String traceId = UUID.randomUUID().toString().substring(0, 8);
+
+        MDC.put("traceId", traceId);
+        try {
+            log.info(">>>> [Request] {} {}", request.getMethod(), request.getRequestURI());
+            filterChain.doFilter(request, response);
+        } finally {
+            log.info("<<<< [Response] Status: {}", response.getStatus());
+            MDC.clear();
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+
+        // 로그를 찍지 않을 정적 리소스 확장자나 경로 패턴 정의
+        return path.startsWith("/css/") ||
+                path.startsWith("/js/") ||
+                path.startsWith("/images/") ||
+                path.endsWith(".ico") ||
+                path.endsWith(".png") ||
+                path.endsWith(".jpg");
+    }
+}
